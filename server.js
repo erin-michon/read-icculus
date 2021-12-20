754abba05fc3844f852b83c66a7a96f9054fd5a2
Erin M <erin.michon@gmail.com>
2021-12-19 20:26:24 -0500

    Add Heroku

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 
-// const {notes} = require('./')
+const {notes} = require('./db/db.json');
 
+app.get('/api/notes', (req,res) => {
+    res.json(notes);
+})
 
 
 app.listen(3001, () => {
